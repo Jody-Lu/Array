fb417ae5c13cc29c1430e08287dba06fbd0c3a6f
Jody-Lu <b00901192@ntu.edu.tw>
2016-07-22 22:04:31 +0800

    write 309, but still has some trouble

--- 309_Best_Time_to_Buy_and_Sell_Stock_with_Colddown/maxProfit.py
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Colddown/maxProfit.py
@@ -3,4 +3,27 @@ class Solution(object):
         """
         :type prices: List[int]
         :rtype: int
+        :profit[i]: max profit till the i-th day
         """
+        days = len(prices)
+        profit = [0] * (days + 1)
+        if days <= 1: return 0
+        
+        for i in xrange(2, days + 1):
+            #print i
+            profit[i] = max(max([prices[i - 1] - prices[j] for j in xrange(i)]), profit[i -1])
+                
+                #profit[i] = max(profit[i - 1], prices[i - 1] - prices[j])
+        
+        print profit
+        return profit[days]
+
+
+sol = Solution()
+prices = [1, 2, 3, 0, 2]
+print sol.maxProfit(prices)
+
+            
+
+
+
