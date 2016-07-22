7067bc23ec8dd6583fcda8ab05f4b1faa4578576
Jody-Lu <b00901192@ntu.edu.tw>
2016-07-22 17:08:49 +0800

    bug fix for wiggle subsequence

--- 376_Wiggle_Subsequence/wiggleMaxLenght_v3.py
+++ 376_Wiggle_Subsequence/wiggleMaxLenght_v3.py
@@ -1,16 +1,27 @@
 class Solution(object):
     def wiggleMaxLength(self, nums):
         length = len(nums)
-        if length <= 2: return length
+        if length <= 1: return length
 
-        top = nums[1] > nums[0]
-        res = 1
+        tops = [True, False]
+        res = 0
 
-        for i in xrange(2, length):
-            if nums[i] == nums[i - 1]:
-                continue
-            tmp = nums[i] > nums[i - 1]
-            if top ^ tmp:
-                res += 1
-                top = tmp
+        # Update for different top (True or False)
+        # Different top has different results.
+        for top in tops:
+            tmp_res = 0
+            for i in xrange(1, length):
+                if nums[i] == nums[i - 1]:
+                    continue
+                tmp = nums[i] > nums[i - 1]
+                if top ^ tmp:
+                    tmp_res += 1
+                    top = tmp
+
+            res = max(res, tmp_res)
+
         return res + 1
+
+sol = Solution()
+nums = [102,101,20,91,156,78,75,142,69,81,46,142,113,163,190,178,13,36,134,54]
+print sol.wiggleMaxLength(nums)
