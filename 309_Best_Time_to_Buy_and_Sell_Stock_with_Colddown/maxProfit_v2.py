0813cec979abeabcd8ace6e67f9e8d7ecbdcc7c8
Jody-Lu <b00901192@ntu.edu.tw>
2016-07-24 00:26:49 +0800

    bug fix

--- 309_Best_Time_to_Buy_and_Sell_Stock_with_Colddown/maxProfit_v2.py
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Colddown/maxProfit_v2.py
@@ -6,20 +6,17 @@ class Solution(object):
         """
         days = len(prices)
         if days < 2: return 0
-        buy = [0] * (days + 1)
+        buy = [-prices[0]] * (days + 1)
         sell = [0] * (days + 1)
         rest = [0] * (days + 1)
 
-        for i in xrange(2, days):
+        for i in xrange(2, days + 1):
             price = prices[i - 1]
             buy[i] = max(rest[i - 1] - price, buy[i - 1])
             sell[i] = max(buy[i - 1] + price, sell[i - 1])
             rest[i] = max(sell[i - 1], buy[i - 1], rest[i - 1])
 
-        print buy
-        print sell
-        print rest
-        return max(buy[-2], sell[-2], rest[-2])
+        return max(buy[-1], sell[-1], rest[-1])
 
 sol = Solution()
 prices = [1, 2, 3, 0, 2]
