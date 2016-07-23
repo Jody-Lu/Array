190e3d8a87486ec8a2b457557107b24d1c07ca4c
Jody-Lu <b00901192@ntu.edu.tw>
2016-07-24 00:49:31 +0800

    complete permutation

--- 46_Permutations/permute.py
+++ 46_Permutations/permute.py
@@ -4,3 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        res = []
+        def dfs(stack, nums):
+            if not nums:
+                res.append(stack)
+                return
+
+            for idx, num in enumerate(nums):
+                dfs(stack + [num], nums[0:idx] + nums[idx + 1:])
+
+        dfs([], nums)
+        return res
+
+sol = Solution()
+nums = [1, 2]
+print sol.permute(nums)
