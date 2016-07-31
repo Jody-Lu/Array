21134058bf60dbfad0a9eb3b04fa6749e61339b4
Jody-Lu <b00901192@ntu.edu.tw>
2016-07-31 17:30:20 +0800

    complete setZeroes

--- 73_set_matrix_zeroes/setZeroes.py
+++ 73_set_matrix_zeroes/setZeroes.py
@@ -1,22 +1,33 @@
 class Solution(object):
-    def zero_row(self, matrix):
-        pass
     def setZeroes(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
+        Use row & col to record the row, col that have '0'
         """
-        flag = False
-        
-        for row in matrix:
-            if 0 in row:
-                flag = True
-                break
-        
-        if flag:
-            for idx in range(len(matrix)):
-                matrix[idx] = [0]*len(matrix[idx])
-        
+        row_num = len(matrix)
+        col_num = len(matrix[0])
+        row = [False] * row_num
+        col = [False] * col_num
+
+        # 1st time survey the matrix 
+        # To find which col/row has zero
+        # We don't need and NxN matrix to memorize it
+        # If a col/row has a zero, then the others are not necessary.
+        for i in xrange(row_num):
+            for j in xrange(col_num):
+                if matrix[i][j] == 0:
+                    row[i] = True
+                    col[j] = True
+
+        # 2nd time survey the matrix
+        for i in xrange(row_num):
+            for j in xrange(col_num):
+                if row[i] or col[j]:
+                    matrix[i][j] = 0
+
+
+
 
 if __name__ == '__main__':
     matrix = [[0,0,0,5],[4,3,1,4],[0,1,1,4],[1,2,1,3],[0,0,1,1]]
