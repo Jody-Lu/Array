18a1125e5e1e97f6e59a559b0df638d925629638
Jody-Lu <b00901192@ntu.edu.tw>
2016-08-20 23:19:45 -0500

    update 172

--- 172_Factorial_Trailing_Zeroes/trailingZeroes.cpp
+++ 172_Factorial_Trailing_Zeroes/trailingZeroes.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 
 using namespace std;
-/*
-class Solution {
-public:
-    int trailingZeroes(int n) {
-        int res = 0;
-        int fac = 5;
-        while(fac <= n) {
-            int tmp = fac;
-            while(tmp % 5 == 0) {
-                res += 1;
-                tmp /= 5;
-            }
-            fac += 5;
-        }
-        return res;
-    }
-};
-*/
+
 class Solution {
 public:
     int trailingZeroes(int n) {
         /* Count number of 5. */
-        return n == 0? 0 : n /5 + trailingZeroes(n/5);
+        int res = 0;
+        while(n) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
     }
+};
+
+
+class Solution {
+    public:
+        int trailingZeroes(int n) {
+            /* Count number of 5. */
+            return n == 0? 0 : n /5 + trailingZeroes(n/5);
+        }
 };
 
 int main() {
