12fc18753a616150dd46074366773da2a1f5f6ae
Jody-Lu <b00901192@ntu.edu.tw>
2016-08-23 23:53:43 -0500

    add divide and conquer ver

--- 53_maximum_subarray/maximum_subarray.cpp
+++ 53_maximum_subarray/maximum_subarray.cpp
@@ -11,8 +11,8 @@ public:
 		int result = INT_MIN, f = 0;
 		for(int i = 0; i < nums.size(); ++i)
 		{
-			f = max(f + nums[i], nums[i]);
-			result = max(result, f); // 把現在較大的 f 存起來，跟下一次的做比較
+			f = max(f + nums[i], nums[i]); // either concatenate or choosing nums[i] 
+            result = max(result, f); 
 		}
 
 		return result;
---
+++ 53_maximum_subarray/maximum_subarray_v2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            return _mergeSort(nums, 0, nums.size() - 1);    
+        }
+    private:
+        int _mergeSort(vector<int>& nums, int begin, int end) {
+            int mid = (begin + end) / 2;
+            int left, right, rMax = nums[mid + 1], lMax = nums[mid];
+            if(begin < end) {
+                left = _mergeSort(nums, begin, mid);
+                right = _mergeSort(nums, mid+1, end);
+                //cout << left << " " << right << endl;
+                //cout << max(left + right, right) << endl; 
+            }
+            else return nums[begin];
+            
+            int tmp = 0;
+            for(int i = mid; i >= begin; i--) {
+                tmp += nums[i];
+                if(tmp > lMax) lMax = tmp;
+            }
+            tmp = 0;
+            for(int i = mid + 1; i <= end; i++) {
+                tmp += nums[i];
+                if(tmp > rMax) rMax = tmp;
+            }
+            return max(max(left, right), lMax+rMax);
+        }
+};
+
+int main() {
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> nums(arr, arr + 9);
+    Solution sol;
+    cout << sol.maxSubArray(nums) << endl;
+}
