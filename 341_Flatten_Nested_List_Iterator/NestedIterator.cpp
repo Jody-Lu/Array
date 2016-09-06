fff6509ea0ab646f050e5ca1c392f58bcaf25784
Jody-Lu <b00901192@ntu.edu.tw>
2016-09-05 22:22:41 -0500

    complete

---
+++ 341_Flatten_Nested_List_Iterator/NestedIterator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class NestedIterator {
+    public:
+        NestedIterator(vector<NestedInteger> &nestedList) {
+            
+        }
+
+        int next() {}
+
+         bool hasNext() {}
+
+
+
+};
+
