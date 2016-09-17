0d5fe418b601934791833cbaa00e3376b1085fcf
Jody-Lu <b00901192@ntu.edu.tw>
2016-09-17 13:21:11 -0500

    add cpp version

--- 384_Shuffle_an_Array/Solution.cpp
+++ 384_Shuffle_an_Array/Solution.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution {
+    private:
+        vector<int> nums;
     public:
-        Solution(vector<int> nums) {}
+        Solution(vector<int> nums) {
+            this -> nums = nums;
+        }
 
         /** Resets the array to its original configuration and return it. */
-        vector<int> reset() {}
+        vector<int> reset() {
+            return nums;
+        }
 
         /** Returns a random shuffling of the array. */
-        vector<int> shuffle() {}
+        vector<int> shuffle() {
+            vector<int> res(nums);
+            for(int i = 0; i < res.size(); i++) {
+                int pos = rand() % (res.size() - i);
+                swap(res[i], res[i + pos]);
+            }
+            return res;
+        }
 };
 
 int main() {
