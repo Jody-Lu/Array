40d31ebdee4d9b3e545f8f1b59305755d32cc302
Jody-Lu <b00901192@ntu.edu.tw>
2016-09-24 13:36:30 -0500

    complete

--- 216_combination_sum/combinationSum3.py
+++ 216_combination_sum/combinationSum3.py
@@ -6,46 +6,24 @@ class Solution(object):
         :rtype List[List[int]]
         """
         result = []
-        stack = []
-        start = 1
-        def helper(k, n, stack, start):
-            if n == 0:
-                if k == 0:
-                    print stack, id(stack)
+
+        def dfs(k, n, stack, start):
+            if k == len(stack):
+                if n == 0:
                     result.append(stack)
-                    return 
-                else:
-                    return
+                return
 
-            for i in range(start, 10):
-                if n < i: return
-                stack.append(i)
-                helper(k-1, n-i, stack, i+1)
-                #helper(k-1, n-i, stack + [i], i+1)
-                stack.pop()
+            for i in xrange(start, 9):
+                if n < (i + 1): return
+                dfs(k, n - (i + 1), stack + [i + 1], i + 1)
 
-        helper(k, n, stack, start)
+        dfs(k, n, [], 0)
         return result
-    """
-    def combinationSum3(self, k, n):
-        ans = []
-        def search(start, cnt, sums, nums):
-            if cnt > k or sums > n:
-                return
-            if cnt == k and sums == n:
-                ans.append(nums)
-                return
-            for x in range(start + 1, 10):
-                search(x, cnt + 1, sums + x, nums + [x])
-        search(0, 0, 0, [])
-        return ans
-    """
+
 if __name__ == '__main__':
     num = []
     sol = Solution()
-    k = 2
-    n = 10
+    k = 3
+    n = 9
     print sol.combinationSum3(k, n)
     
-    #sol.test(num)
-    #print num
