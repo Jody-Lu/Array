096f3ccbc36f58490f6a0683bc2c390c13bf74ee
Jody-Lu <b00901192@ntu.edu.tw>
2016-10-06 18:05:11 -0500

    add DP version

--- 410_Split_Array_Largest_Sum/splitArray_DP.cpp
+++ 410_Split_Array_Largest_Sum/splitArray_DP.cpp
@@ -8,19 +8,33 @@ class Solution {
         int splitArray(vector<int> &nums, int m) {
             int n = nums.size();
             // DP[i][j]: the solution to divide nums[j] ~ nums[L-1] into i parts.
-            vector<vector<int> > DP(m, vector<int>(n, 0));
+            vector<int> dp(n, 0);
             vector<int> sums(n + 1, 0);
-            
-            for(int i = 1; i <= n; i++)
-                sums[i] = sums[i - 1] + nums[i - 1];
 
-            // Divide into one part --> m = 0
             for(int i = 0; i < n; i++)
-                DP[0][i] = sums[n] - sums[i];
+                sums[i + 1] = sums[i] + nums[i];
 
 
-            
-            
+            for(int i = 0; i < n; i++)
+                dp[i] = sums[n] - sums[i];
 
+            for(int s = 1; s < m; s++) {
+                for(int i = 0; i < (n - s); i++) {
+                    dp[i] = INT_MAX;
+                    for(int j = i + 1; j <= n - s; j++) {
+                        int t = max(dp[j], sums[j] - sums[i]);
+                        if(t <= dp[i])
+                            dp[i] = t;
+                        else
+                            break;
+                    }
+
+                }
+            }
+            return dp[0];
         }
 };
+
+int main() {
+    return 0;
+}
