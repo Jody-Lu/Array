55c2b0062518bebbb847d2cdcd370c0b71bd09e2
Jody-Lu <b00901192@ntu.edu.tw>
2016-10-07 16:47:07 -0500

    add comment

--- 8_String_to_Integer_Atoi/myAtoi.cpp
+++ 8_String_to_Integer_Atoi/myAtoi.cpp
@@ -10,23 +10,30 @@ class Solution {
             long res = 0;
             int sign = 1;
             int count = 0; // Count number of digits.
-            while(s[idx] == ' ') idx++; // Remove spaces.
+            while(s[idx] == ' ') idx++; // skip spaces at the beginnign.
+
             /* Check the first digit is either '+' or '-'. */
+            /* Positive or Negative number. */
             if(s[idx] == '+') idx++; 
-            else if(s[idx] == '-') {
+            else if(s[idx] == '-') 
+            {
                 idx++;
                 sign = -1;
             }
-            while(idx < s.size()) {
-                if(isdigit(s[idx])) {
-                    res *= 10;
-                    res += (int)(s[idx] - '0');
+
+            /* Get the digits. */
+            while(idx < s.size()) 
+            {
+                if(isdigit(s[idx])) 
+                {
+                    res = res * 10 + (s[idx] - '0');
                     idx++;
                     count++;
                 }
                 else
                     break;
             }
+
             /* If #digits > 10, overflow.*/
             if(count > 10 || res > INT_MAX)
                 return sign == 1? INT_MAX : INT_MIN;
@@ -37,7 +44,6 @@ class Solution {
             return 0;
         }
 };
-
 int main() {
     string s = "   a1776a";
     Solution sol;
