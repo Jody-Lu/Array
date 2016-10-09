4039432304a9db656ad42a8a6d0ff8901ab3d29d
Jody-Lu <b00901192@ntu.edu.tw>
2016-10-09 11:13:28 -0500

    update

---
+++ 105_construct_binary_tree_from_preorder_and_inorder_traversal/buildTree_v3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+    public:
+        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+            return dfs(inorder, preorder, 0, inorder.size() - 1);
+        }
+    private:
+        TreeNode* dfs(vector<int>& in, vector<int>& pre, int inStrt, int inEnd) {
+            static int preIndex = 0;
+
+            if(inStrt > inEnd)
+                return NULL;
+
+            // Find the root at this stage.
+            TreeNode* tNode = new TreeNode(pre[preIndex++]);
+
+            // At this stage, this node is a leaf node.
+            if(inStrt == inEnd)
+                return tNode;
+
+            // Not a leaf node, find the left and right subtree
+            // We define a search function here to help us find the root in inOrder
+            int inIndex = search(in, inStrt, inEnd, tNode->val);
+
+            tNode -> left = dfs(in, pre, inStrt, inIndex - 1);
+            tNode -> right = dfs(in, pre, inIndex + 1, inEnd);
+            return tNode;
+        }
+        int search(vector<int>& v, int strt, int end, int target) {
+            int i;
+            for(i = strt; i <= end; i++) {
+                if(v[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+
+};
+
+int main() {
+    return 0;
+}
